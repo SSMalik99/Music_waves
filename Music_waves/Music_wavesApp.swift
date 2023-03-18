3a16fbca1b3cb6a4971c5b2429d8cbb75b2253b7
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2023-03-17 21:31:25 -0400

    Initial Commit

---
+++ Music_waves/Music_wavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_wavesApp.swift
+//  Music_waves
+//
+//  Created by Saravjeet Singh on 2023-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Music_wavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
